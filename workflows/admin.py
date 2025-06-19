6c324d6ce13e4716efee2f5523f3c0c8f8b94002
digital-pet <47679051+digital-pet@users.noreply.github.com>
2025-06-18 23:23:40 -0400

    Improve admin views

--- workflows/admin.py
+++ workflows/admin.py
@@ -1,5 +1,6 @@
 """Admin models"""
 
+from allianceauth.services.admin import ServicesUserAdmin
 from .models import Wizard, Check, Step, CheckFilter, ActionItem, StepCompletion
 
 # Django
@@ -7,9 +8,23 @@ from django.contrib import admin  # noqa: F401
 
 # Register your models here.
 
-admin.site.register(Wizard)
+
+@admin.register(Wizard)
+class WizardAdmin(admin.ModelAdmin):
+    list_display = ['name', 'comment', 'description', 'permalink', 'auto_assigned']
+    filter_horizontal = ['states', 'groups', 'corporations', 'alliances', 'factions', 'characters']
+
+
 admin.site.register(Step)
 admin.site.register(Check)
 admin.site.register(CheckFilter)
-admin.site.register(ActionItem)
-admin.site.register(StepCompletion)
+
+@admin.register(ActionItem)
+class ActionItemAdmin(ServicesUserAdmin):
+    search_fields = ServicesUserAdmin.search_fields + ('wizard',)
+    list_display = ServicesUserAdmin.list_display + ('wizard', 'completed')
+
+@admin.register(StepCompletion)
+class StepCompletionAdmin(ServicesUserAdmin):
+    search_fields = ServicesUserAdmin.search_fields + ('wizard','step')
+    list_display = ServicesUserAdmin.list_display + ('wizard', 'step')
